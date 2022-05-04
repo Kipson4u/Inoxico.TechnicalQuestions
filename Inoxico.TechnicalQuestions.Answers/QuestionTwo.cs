ca717d6aa17fb486a14c1162d7bb70618ef6b709
Kipson Tshidino <fhumulani.tshidino@lexisnexis.co.za>
2022-05-04 07:42:34 +0200

    Code Clean Up and Comments

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -2,51 +2,51 @@
 namespace Inoxico.TechnicalQuestions.Answers
 {
     public class QuestionTwo
-    {
+    {        
         public static int GetPitDepth(int[] points)
-        {
-            //o 0 ≤ P > Q < R;
-            //o sequence[A[P], A[P + 1], ..., A[Q]] is strictly decreasing,
-            //i.e.A[P] > A[P + 1] = A[Q];
-            //o sequence A[Q], A[Q + 1], ..., A[R] is strictly increasing,
-            //i.e.A[Q] < A[Q + 1] = A[R].
-            //o The depth of a pit(P, Q, R) is the number min{ A[P] − A[Q], A[R] − A[Q]}.
-           
+        {                       
             int depthOfdeepestPit = -1;
             int arraySize = points.Length - 1;
 
+            // Check if the array is long enough to store a pit
             if (arraySize < 2)
                 return depthOfdeepestPit;
 
-
             for (int P = 0; P < arraySize - 2; P++)
-            {                
+            {
+                //P_Point is the starting point
                 int P_Point = points[P];
                 int Q = P + 1;
 
+                //Strictly decreasing sequence
                 while (points[Q] < points[Q - 1] && Q < arraySize - 1)
                     Q++;
 
+                //Check for flat line from decreasing sequence (No flat line)
                 if (Q == P + 1)
                     continue;
 
                 Q = Q - 1;
-                
+
+                //Q_Point is the lowest point
                 int Q_Point = points[Q];
                 int R = Q + 1;
 
+                //Strictly increasing sequence
                 while (points[R] > points[R - 1] && R < arraySize)
                     R++;
 
+                //Check for flat line from increasing sequence (No flat line)
                 if (R == Q + 1)
                     continue;
 
                 R = R - 1;
-                
+
+                //R_Point is the last point
                 int R_Point = points[R];
                                 
                 if (R_Point >= 0 && P_Point >= 0)
-                    depthOfdeepestPit =Math.Max(depthOfdeepestPit, Math.Min(P_Point - Q_Point, R_Point - Q_Point));
+                    depthOfdeepestPit = Math.Max(depthOfdeepestPit, Math.Min(P_Point - Q_Point, R_Point - Q_Point));
 
             }
 
