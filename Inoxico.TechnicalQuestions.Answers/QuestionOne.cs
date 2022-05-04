bdbf1bc3991dc6f676265f3bf5acfb54a078fc3b
Kipson Tshidino <fhumulani.tshidino@lexisnexis.co.za>
2022-05-04 06:49:41 +0200

    Validate  the input string length

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -1,17 +1,25 @@
 ﻿
+using System.Text.RegularExpressions;
+
 namespace Inoxico.TechnicalQuestions.Answers
 {
     public class QuestionOne
     {
         public static int GetLongestSentance(string s)
         {
+            if (s.Length < 1 || s.Length > 100)
+                throw new ArgumentOutOfRangeException(nameof(s), $"The length of String must be within the range 1 to 100");
+
             string[] sentences = s.Split('.', '!', '?');
             int mostSentenceWords = 0;
 
             foreach (string sentence in sentences)
             {
                 string[] words = sentence.Trim().Split(' ');
-                
+
+                //A sentence can be divided into words by splitting it at spaces. A sentence without words
+                //is valid, but a valid word must contain at least one letter.
+                //words = words.Where(word => !Regex.IsMatch(word, @"/[a-zA-Z]/")).ToArray();
                 if (words.Length > mostSentenceWords)
                     mostSentenceWords = words.Length;
             }
