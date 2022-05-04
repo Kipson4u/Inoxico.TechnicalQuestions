0b5be993e68d0ac59c7a26516ca19c5a412109ef
Kipson Tshidino <fhumulani.tshidino@lexisnexis.co.za>
2022-05-04 06:39:18 +0200

    GetLongestSentance impementation return the most sentence words count

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -5,7 +5,18 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            string[] sentences = s.Split('.', '!', '?');
+            int mostSentenceWords = 0;
+
+            foreach (string sentence in sentences)
+            {
+                string[] words = sentence.Trim().Split(' ');
+                
+                if (words.Length > mostSentenceWords)
+                    mostSentenceWords = words.Length;
+            }
+
+            return mostSentenceWords;
         }
     }
 }
